ce2d99eff093c2257a680399b7f245bec1b63412
Ben Holden <bholden1@andrew.cmu.edu>
2015-10-06 17:03:32 -0400

    Solved up to HW2, 2.5

--- HW2/project2/simpleDemo.m
+++ HW2/project2/simpleDemo.m
@@ -11,13 +11,31 @@ policies = {policyConstant(), policyRandom()};
 policy_names = {'policyConstant', 'policyRandom'};
 
 %% Run the policies on the game
-figure;
-hold on;
+reward_m = zeros(length(policies),game.totalRounds);
+action_m = zeros(length(policies),game.totalRounds);
+regret_m = zeros(length(policies),game.totalRounds);
 for k = 1:length(policies)
     policy = policies{k};
     game.resetGame();
     [reward, action, regret] = game.play(policy);
     fprintf('Policy: %s Reward: %.2f\n', class(policy), sum(reward));
-    plot(regret);
+    reward_m(k,:) = reward;
+    action_m(k,:) = action;
+    regret_m(k,:) = regret;
+end
+figure(1)
+hold on
+title('Regret');
+for k = 1:length(policies)
+    plot(regret_m(k,:));
 end
 legend(policy_names);
+hold off
+figure(2)
+hold on
+title('Action');
+for k = 1:length(policies)
+    plot(action_m(k,:));
+end
+legend(policy_names);
+hold off

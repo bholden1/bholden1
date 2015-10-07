648358d9e3fd0a9a10db275e0f00bb49d8ecb1fa
bholden1 <bholden1@andrew.cmu.edu>
2015-10-07 13:59:47 -0400

    Start 3.1.1

--- HW2/project2/policyGWM.m
+++ HW2/project2/policyGWM.m
@@ -4,6 +4,8 @@ classdef policyGWM < Policy
     properties
         nbActions % number of bandit actions
         % Add more member variables as needed
+        weights % weights for the bandit actions
+        t % track the step number for eta updates
     end
     
     methods
@@ -11,7 +13,8 @@ classdef policyGWM < Policy
         function init(self, nbActions)
             % Initialize any member variables
             self.nbActions = nbActions;
-            
+            self.weights = ones(1,nbActions);
+            self.t = 1;
             % Initialize other variables as needed
 
         end
@@ -23,13 +26,14 @@ classdef policyGWM < Policy
         
         function getReward(self, reward)
             % Update the weights
-            
+            eta = sqrt(log10(self.nbActions)/self.t);
+            self.t = self.t + 1;
             % First we create the loss vector for GWM
             lossScalar = 1 - reward; % This is loss of the chosen action
             lossVector = zeros(1,self.nbActions);
             lossVector(self.lastAction) = lossScalar;
-            
             % Do more stuff here using loss Vector
+            self.weights = self.weights.*exp(-eta*lossVector);
         end        
     end
 end
